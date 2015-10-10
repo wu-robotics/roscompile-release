b0da32e8a25654c8898cfcd8d8657adec914afd4
David Lu <davidvlu@gmail.com>
2015-10-09 20:51:47 -0400

    Rebase from 'rpm/indigo/roscompile'

--- src/roscompile/source.py
+++ src/roscompile/source.py
@@ -1,19 +1,24 @@
 import re
-from roscompile.package_list import is_package
+import os
+from roscompile.resource_list import is_package, is_message, is_service
 
 PKG = '([^\.;]+)(\.?[^;]*)?'
 PYTHON1 = '^import ' + PKG
 PYTHON2 = 'from ' + PKG + ' import .*'
-CPLUS = '#include\s*[<\\"]([^/]*)/?([^/]*)[>\\"]'
+CPLUS = re.compile('#include\s*[<\\"]([^/]*)/?([^/]*)[>\\"]')
 
-EXPRESSIONS = [re.compile(PYTHON1), re.compile(PYTHON2), re.compile(CPLUS)]
+EXPRESSIONS = [re.compile(PYTHON1), re.compile(PYTHON2), CPLUS]
 
 PLUGIN_PATTERN = 'PLUGINLIB_EXPORT_CLASS\(([^:]+)::([^,]+), ([^:]+)::([^,]+)\)'
 PLUGIN_RE = re.compile(PLUGIN_PATTERN)
 
 class Source:
-    def __init__(self, fn):
+    def __init__(self, fn, root=None):
         self.fn = fn
+        if root:
+            self.rel_fn = fn.replace(root + '/', '')
+        else:
+            self.rel_fn = fn
         self.lines = map(str.strip, open(fn, 'r').readlines())
         self.python = '.py' in self.fn or (len(self.lines)>0 and 'python' in self.lines[0])  
         
@@ -27,6 +32,19 @@ class Source:
                         d.add(m.group(1))
         return sorted(list(d))
         
+    def get_message_dependencies(self):
+        d = set()
+        for line in self.lines:
+            m = CPLUS.search(line)
+            if m:
+                pkg, name = m.groups()
+                if len(name)==0 or name[-2:]!='.h':
+                    continue
+                name = name.replace('.h', '')
+                if is_message(pkg, name) or is_service(pkg, name):
+                    d.add(pkg)
+        return sorted(list(d))
+        
     def get_plugins(self):
         a = []
         for line in self.lines:
@@ -34,6 +52,9 @@ class Source:
             if m:
                 a.append( m.groups() )
         return a
+
+    def is_executable(self):
+        return os.access(self.fn, os.X_OK)
 
     def __repr__(self):
         return self.fn
